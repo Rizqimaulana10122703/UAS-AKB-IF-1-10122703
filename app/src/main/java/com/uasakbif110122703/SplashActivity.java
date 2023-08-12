d5c30e6fb582c345cc827336166d9f843e9068fd
Rizqimaulana10122703 <rizqi.10122703@mahasiswa.unikom.ac.id>
2023-08-12 15:16:20 +0700

    first commit

---
+++ app/src/main/java/com/uasakbif110122703/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.uasakbif110122703;
+/**
+ * NIM : 10122703
+ * Nama : Mochammad Rizqi Maulana
+ * Kelas : IF 1
+ */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("CatatanHarian");
+        }
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
